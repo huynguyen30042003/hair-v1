0b270354b406243ef5b58aaca1ddad57d4a93bc7
HiamNos <ttinh2852@gmail.com>
2024-06-07 08:11:58 +0700

    hh

--- app/not-found.js
+++ app/not-found.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const notFound = () => {
   return (
-    <div className='pt-6'>not-foundjbbjlbjhl
+    <div className='pt-6'>not-foundjbbjlbjhl bbkkbkbjk
     </div>
   )
 }
